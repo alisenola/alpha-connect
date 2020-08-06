d0b6817bc72dd63b798207957c8be06255f44b15
Laurent Meunier <laurmeun.business@gmail.com>
2020-08-06 16:14:18 +0200

    feat: huobi perpetual listener + executor

---
+++ exchanges/huobip/huobip_executor.go
@@ -0,0 +1,232 @@
+package huobip
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/log"
+	"github.com/gogo/protobuf/types"
+	"gitlab.com/alphaticks/alphac/enum"
+	"gitlab.com/alphaticks/alphac/exchanges/interface"
+	"gitlab.com/alphaticks/alphac/jobs"
+	"gitlab.com/alphaticks/alphac/models"
+	"gitlab.com/alphaticks/alphac/models/messages"
+	"gitlab.com/alphaticks/alphac/utils"
+	"gitlab.com/alphaticks/xchanger/constants"
+	"gitlab.com/alphaticks/xchanger/exchanges"
+	"gitlab.com/alphaticks/xchanger/exchanges/huobip"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+	"time"
+)
+
+type Executor struct {
+	client      *http.Client
+	securities  []*models.Security
+	rateLimit   *exchanges.RateLimit
+	queryRunner *actor.PID
+	logger      *log.Logger
+}
+
+func NewExecutor() actor.Actor {
+	return &Executor{
+		client:      nil,
+		rateLimit:   nil,
+		queryRunner: nil,
+		logger:      nil,
+	}
+}
+
+func (state *Executor) Receive(context actor.Context) {
+	_interface.ExchangeExecutorReceive(state, context)
+}
+
+func (state *Executor) GetLogger() *log.Logger {
+	return state.logger
+}
+
+func (state *Executor) Initialize(context actor.Context) error {
+	state.logger = log.New(
+		log.InfoLevel,
+		"",
+		log.String("ID", context.Self().Id),
+		log.String("type", reflect.TypeOf(*state).String()))
+
+	state.client = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 1024,
+			TLSHandshakeTimeout: 10 * time.Second,
+		},
+		Timeout: 10 * time.Second,
+	}
+	state.rateLimit = exchanges.NewRateLimit(10, time.Second)
+	props := actor.PropsFromProducer(func() actor.Actor {
+		return jobs.NewAPIQuery(state.client)
+	})
+	state.queryRunner = context.Spawn(props)
+
+	return state.UpdateSecurityList(context)
+}
+
+func (state *Executor) Clean(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) UpdateSecurityList(context actor.Context) error {
+	request, weight, err := huobip.GetSwapInfo()
+	if err != nil {
+		return err
+	}
+
+	if state.rateLimit.IsRateLimited() {
+		return fmt.Errorf("rate limited")
+	}
+	state.rateLimit.Request(weight)
+
+	resp, err := state.client.Do(request)
+	if err != nil {
+		return err
+	}
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+			err := fmt.Errorf(
+				"http client error: %d %s",
+				resp.StatusCode,
+				string(response))
+			return err
+		} else if resp.StatusCode >= 500 {
+			err := fmt.Errorf(
+				"http server error: %d %s",
+				resp.StatusCode,
+				string(response))
+			return err
+		} else {
+			err := fmt.Errorf("%d %s",
+				resp.StatusCode,
+				string(response))
+			return err
+		}
+	}
+
+	type Data struct {
+		Status string            `json:"status"`
+		Data   []huobip.SwapInfo `json:"data"`
+	}
+	var data Data
+	err = json.Unmarshal(response, &data)
+	if err != nil {
+		err = fmt.Errorf(
+			"error unmarshaling response: %v",
+			err)
+		return err
+	}
+	if data.Status != "ok" {
+		err = fmt.Errorf(
+			"got wrong status: %s",
+			data.Status)
+		return err
+	}
+
+	var securities []*models.Security
+	for _, symbol := range data.Data {
+		splits := strings.Split(symbol.ContractCode, "-")
+		baseStr := strings.ToUpper(splits[0])
+		quoteStr := strings.ToUpper(splits[1])
+
+		baseCurrency, ok := constants.SYMBOL_TO_ASSET[baseStr]
+		if !ok {
+			//state.logger.Info(fmt.Sprintf("unknown currency %s", baseStr))
+			continue
+		}
+		quoteCurrency, ok := constants.SYMBOL_TO_ASSET[quoteStr]
+		if !ok {
+			//state.logger.Info(fmt.Sprintf("unknown currency %s", quoteStr))
+			continue
+		}
+		security := models.Security{}
+		security.Symbol = symbol.ContractCode
+		security.Underlying = &baseCurrency
+		security.QuoteCurrency = &quoteCurrency
+		security.Enabled = symbol.ContractStatus == 1
+		security.Exchange = &constants.HUOBIP
+		security.SecurityType = enum.SecurityType_CRYPTO_PERP
+		security.SecurityID = utils.SecurityID(security.SecurityType, security.Symbol, security.Exchange.Name)
+		security.MinPriceIncrement = symbol.PriceTick
+		security.RoundLot = 1
+		security.IsInverse = true
+		security.Multiplier = &types.DoubleValue{Value: symbol.ContractSize}
+		securities = append(securities, &security)
+	}
+	state.securities = securities
+
+	context.Send(context.Parent(), &messages.SecurityList{
+		ResponseID: uint64(time.Now().UnixNano()),
+		Success:    true,
+		Securities: state.securities})
+
+	return nil
+}
+
+func (state *Executor) OnSecurityListRequest(context actor.Context) error {
+	// Get http request and the expected response
+	msg := context.Message().(*messages.SecurityListRequest)
+
+	context.Respond(&messages.SecurityList{
+		RequestID:  msg.RequestID,
+		ResponseID: uint64(time.Now().UnixNano()),
+		Success:    true,
+		Securities: state.securities})
+
+	return nil
+}
+
+func (state *Executor) OnMarketDataRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnOrderStatusRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnPositionsRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnBalancesRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnNewOrderSingleRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnNewOrderBulkRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnOrderReplaceRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnOrderBulkReplaceRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnOrderCancelRequest(context actor.Context) error {
+	return nil
+}
+
+func (state *Executor) OnOrderMassCancelRequest(context actor.Context) error {
+	return nil
+}
---
+++ exchanges/huobip/huobip_listener.go
@@ -0,0 +1,420 @@
+package huobip
+
+import (
+	"errors"
+	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/log"
+	"gitlab.com/alphaticks/alphac/models"
+	"gitlab.com/alphaticks/alphac/models/messages"
+	"gitlab.com/alphaticks/alphac/utils"
+	"gitlab.com/alphaticks/gorderbook"
+	"gitlab.com/alphaticks/xchanger/exchanges/huobip"
+	"math"
+	"reflect"
+	"sort"
+	"time"
+)
+
+type checkSockets struct{}
+
+type InstrumentData struct {
+	orderBook      *gorderbook.OrderBookL2
+	seqNum         uint64
+	lastUpdateID   uint64
+	lastUpdateTime uint64
+	lastHBTime     time.Time
+	lastAggTradeTs uint64
+}
+
+type Listener struct {
+	ws              *huobip.Websocket
+	security        *models.Security
+	instrumentData  *InstrumentData
+	executorManager *actor.PID
+	mediator        *actor.PID
+	logger          *log.Logger
+	lastPingTime    time.Time
+	socketTicker    *time.Ticker
+}
+
+func NewListenerProducer(security *models.Security) actor.Producer {
+	return func() actor.Actor {
+		return NewListener(security)
+	}
+}
+
+func NewListener(security *models.Security) actor.Actor {
+	return &Listener{
+		ws:              nil,
+		security:        security,
+		instrumentData:  nil,
+		executorManager: nil,
+		mediator:        nil,
+		logger:          nil,
+		socketTicker:    nil,
+	}
+}
+
+func (state *Listener) Receive(context actor.Context) {
+	switch context.Message().(type) {
+	case *actor.Started:
+		if err := state.Initialize(context); err != nil {
+			state.logger.Error("error initializing", log.Error(err))
+			panic(err)
+		}
+		state.logger.Info("actor started")
+
+	case *actor.Stopping:
+		if err := state.Clean(context); err != nil {
+			state.logger.Error("error stopping", log.Error(err))
+			panic(err)
+		}
+		state.logger.Info("actor stopping")
+
+	case *actor.Stopped:
+		state.logger.Info("actor stopped")
+
+	case *actor.Restarting:
+		if err := state.Clean(context); err != nil {
+			state.logger.Error("error restarting", log.Error(err))
+			// Attention, no panic in restarting or infinite loop
+		}
+		state.logger.Info("actor restarting")
+
+	case *messages.MarketDataRequest:
+		if err := state.OnMarketDataRequest(context); err != nil {
+			state.logger.Error("error processing OnMarketDataRequest", log.Error(err))
+			panic(err)
+		}
+
+	case *huobip.WebsocketMessage:
+		if err := state.onWebsocketMessage(context); err != nil {
+			state.logger.Error("error processing websocket message", log.Error(err))
+			panic(err)
+		}
+
+	case *checkSockets:
+		if err := state.checkSockets(context); err != nil {
+			state.logger.Error("error checking socket", log.Error(err))
+			panic(err)
+		}
+	}
+}
+
+func (state *Listener) Initialize(context actor.Context) error {
+	state.logger = log.New(
+		log.InfoLevel,
+		"",
+		log.String("ID", context.Self().Id),
+		log.String("type", reflect.TypeOf(*state).String()),
+		log.String("exchange", state.security.Exchange.Name),
+		log.String("symbol", state.security.Symbol))
+
+	state.mediator = actor.NewLocalPID("data_broker")
+	state.executorManager = actor.NewLocalPID("exchange_executor_manager")
+	state.lastPingTime = time.Now()
+
+	state.instrumentData = &InstrumentData{
+		orderBook:      nil,
+		seqNum:         uint64(time.Now().UnixNano()),
+		lastUpdateID:   0,
+		lastUpdateTime: 0,
+		lastHBTime:     time.Now(),
+	}
+
+	if err := state.subscribeInstrument(context); err != nil {
+		return fmt.Errorf("error subscribing to order book: %v", err)
+	}
+
+	socketTicker := time.NewTicker(5 * time.Second)
+	state.socketTicker = socketTicker
+	go func(pid *actor.PID) {
+		for {
+			select {
+			case _ = <-socketTicker.C:
+				context.Send(pid, &checkSockets{})
+			case <-time.After(10 * time.Second):
+				// timer stopped, we leave
+				return
+			}
+		}
+	}(context.Self())
+
+	return nil
+}
+
+func (state *Listener) Clean(context actor.Context) error {
+	if state.ws != nil {
+		if err := state.ws.Disconnect(); err != nil {
+			state.logger.Info("error disconnecting socket", log.Error(err))
+		}
+	}
+
+	if state.socketTicker != nil {
+		state.socketTicker.Stop()
+		state.socketTicker = nil
+	}
+
+	return nil
+}
+
+func (state *Listener) subscribeInstrument(context actor.Context) error {
+	if state.ws != nil {
+		_ = state.ws.Disconnect()
+	}
+
+	ws := huobip.NewWebsocket()
+	if err := ws.Connect(); err != nil {
+		return fmt.Errorf("error connecting to huobi websocket: %v", err)
+	}
+
+	if err := ws.SubscribeMarketDepth(state.security.Symbol, huobip.WSOBLevel150, true); err != nil {
+		return fmt.Errorf("error subscribing to orderbook for %s", state.security.Symbol)
+	}
+
+	var ob *gorderbook.OrderBookL2
+	nTries := 0
+	for nTries < 100 {
+		if !ws.ReadMessage() {
+			return fmt.Errorf("error reading message: %v", ws.Err)
+		}
+
+		switch ws.Msg.Message.(type) {
+		case huobip.WSMarketDepthTick:
+			res := ws.Msg.Message.(huobip.WSMarketDepthTick)
+			if res.Event != "snapshot" {
+				return fmt.Errorf("was expecting snapshot as first event, got %s", res.Event)
+			}
+			bids, asks := res.ToBidAsk()
+			tickPrecision := uint64(math.Ceil(1. / state.security.MinPriceIncrement))
+			lotPrecision := uint64(math.Ceil(1. / state.security.RoundLot))
+			ob = gorderbook.NewOrderBookL2(
+				tickPrecision,
+				lotPrecision,
+				10000)
+			ob.Sync(bids, asks)
+			ts := uint64(ws.Msg.ClientTime.UnixNano()) / 1000000
+			state.instrumentData.orderBook = ob
+			state.instrumentData.lastUpdateTime = ts
+			state.instrumentData.lastUpdateID = res.Version
+			state.instrumentData.seqNum = uint64(time.Now().UnixNano())
+			nTries = 100
+
+		case huobip.WSError:
+			err := fmt.Errorf("error getting orderbook: %s", ws.Msg.Message.(huobip.WSError).ErrMsg)
+			return err
+		}
+		nTries += 1
+	}
+
+	if ob == nil {
+		return fmt.Errorf("error getting orderbook")
+	}
+
+	if err := ws.SubscribeMarketTradeDetail(state.security.Symbol); err != nil {
+		return fmt.Errorf("error subscribing to trades for %s", state.security.Symbol)
+	}
+
+	state.ws = ws
+
+	go func(ws *huobip.Websocket, pid *actor.PID) {
+		for ws.ReadMessage() {
+			actor.EmptyRootContext.Send(pid, ws.Msg)
+		}
+	}(ws, context.Self())
+
+	return nil
+}
+
+func (state *Listener) OnMarketDataRequest(context actor.Context) error {
+	msg := context.Message().(*messages.MarketDataRequest)
+	response := &messages.MarketDataResponse{
+		RequestID:  msg.RequestID,
+		ResponseID: uint64(time.Now().UnixNano()),
+		SeqNum:     state.instrumentData.seqNum,
+		Success:    true,
+	}
+	if msg.Aggregation == models.L2 {
+		snapshot := &models.OBL2Snapshot{
+			Bids:      state.instrumentData.orderBook.GetBids(0),
+			Asks:      state.instrumentData.orderBook.GetAsks(0),
+			Timestamp: utils.MilliToTimestamp(state.instrumentData.lastUpdateTime),
+		}
+		response.SnapshotL2 = snapshot
+	}
+
+	context.Respond(response)
+	return nil
+}
+
+func (state *Listener) onWebsocketMessage(context actor.Context) error {
+	msg := context.Message().(*huobip.WebsocketMessage)
+	switch msg.Message.(type) {
+
+	case error:
+		return fmt.Errorf("OB socket error: %v", msg)
+
+	case huobip.WSMarketDepthTick:
+		ts := uint64(msg.ClientTime.UnixNano() / 1000000)
+		update := msg.Message.(huobip.WSMarketDepthTick)
+
+		instr := state.instrumentData
+		if update.Version <= instr.lastUpdateID {
+			break
+		}
+
+		// This, implies that instr.lastUpdateID > seqNum
+		// We want update.SeqNum > instr.lastUpdateID
+		if instr.lastUpdateID+1 != update.Version {
+			state.logger.Info("error processing ob update for "+update.Symbol, log.Error(fmt.Errorf("out of order sequence")))
+			return state.subscribeInstrument(context)
+		}
+
+		obDelta := &models.OBL2Update{
+			Levels:    nil,
+			Timestamp: utils.MilliToTimestamp(ts),
+			Trade:     false,
+		}
+
+		for _, bid := range update.Bids {
+			level := gorderbook.OrderBookLevel{
+				Price:    bid.Price,
+				Quantity: bid.Quantity,
+				Bid:      true,
+			}
+			instr.orderBook.UpdateOrderBookLevel(level)
+			obDelta.Levels = append(obDelta.Levels, level)
+		}
+		for _, ask := range update.Asks {
+			level := gorderbook.OrderBookLevel{
+				Price:    ask.Price,
+				Quantity: ask.Quantity,
+				Bid:      false,
+			}
+			instr.orderBook.UpdateOrderBookLevel(level)
+			obDelta.Levels = append(obDelta.Levels, level)
+		}
+
+		if state.instrumentData.orderBook.Crossed() {
+			state.logger.Info("crossed orderbook", log.Error(errors.New("crossed")))
+			return state.subscribeInstrument(context)
+		}
+
+		context.Send(context.Parent(), &messages.MarketDataIncrementalRefresh{
+			UpdateL2: obDelta,
+			SeqNum:   state.instrumentData.seqNum + 1,
+		})
+		state.instrumentData.seqNum += 1
+
+		instr.lastUpdateTime = ts
+		instr.lastUpdateID = update.Version
+		//state.postSnapshot(context)
+
+	case huobip.WSMarketTradeDetailTick:
+		ts := uint64(msg.ClientTime.UnixNano() / 1000000)
+		trades := msg.Message.(huobip.WSMarketTradeDetailTick)
+		if len(trades.Data) == 0 {
+			break
+		}
+
+		sort.Slice(trades.Data, func(i, j int) bool {
+			return trades.Data[i].Timestamp < trades.Data[j].Timestamp
+		})
+
+		var aggTrade *models.AggregatedTrade
+		var aggHelpR uint64 = 0
+		for _, trade := range trades.Data {
+			aggHelp := trade.Timestamp * 10
+			// do that so new agg trade if side changes
+			if trade.Direction == "sell" {
+				aggHelp += 1
+			}
+			if aggTrade == nil || aggHelpR != aggHelp {
+
+				if aggTrade != nil {
+					// Send aggregate trade
+					context.Send(context.Parent(), &messages.MarketDataIncrementalRefresh{
+						Trades: []*models.AggregatedTrade{aggTrade},
+						SeqNum: state.instrumentData.seqNum + 1,
+					})
+					state.instrumentData.seqNum += 1
+					state.instrumentData.lastAggTradeTs = ts
+				}
+
+				if ts <= state.instrumentData.lastAggTradeTs {
+					ts = state.instrumentData.lastAggTradeTs + 1
+				}
+				aggTrade = &models.AggregatedTrade{
+					Bid:         trade.Direction == "sell",
+					Timestamp:   utils.MilliToTimestamp(ts),
+					AggregateID: trade.ID,
+					Trades:      nil,
+				}
+				aggHelpR = aggHelp
+			}
+
+			trd := models.Trade{
+				Price:    trade.Price,
+				Quantity: trade.Amount,
+				ID:       trade.ID,
+			}
+
+			aggTrade.Trades = append(aggTrade.Trades, trd)
+		}
+		if aggTrade != nil {
+			// Send aggregate trade
+			context.Send(context.Parent(), &messages.MarketDataIncrementalRefresh{
+				Trades: []*models.AggregatedTrade{aggTrade},
+				SeqNum: state.instrumentData.seqNum + 1,
+			})
+			state.instrumentData.seqNum += 1
+			state.instrumentData.lastAggTradeTs = ts
+		}
+
+	case huobip.WSSubscribeResponse:
+		// pass
+
+	case huobip.WSPing:
+		msg := msg.Message.(huobip.WSPing)
+		if err := state.ws.Pong(msg.Ping); err != nil {
+			return fmt.Errorf("error sending pong to websocket")
+		}
+
+	case huobip.WSError:
+		msg := msg.Message.(huobip.WSError)
+		state.logger.Info("got WSError message",
+			log.String("message", msg.ErrMsg),
+			log.String("code", msg.ErrCode))
+
+	default:
+		state.logger.Info("received unknown message",
+			log.String("message_type",
+				reflect.TypeOf(msg.Message).String()))
+	}
+
+	return nil
+}
+
+func (state *Listener) checkSockets(context actor.Context) error {
+	// If haven't sent anything for 2 seconds, send heartbeat
+	if time.Now().Sub(state.instrumentData.lastHBTime) > 2*time.Second {
+		// Send an empty refresh
+		context.Send(context.Parent(), &messages.MarketDataIncrementalRefresh{
+			SeqNum: state.instrumentData.seqNum + 1,
+		})
+		state.instrumentData.seqNum += 1
+		state.instrumentData.lastHBTime = time.Now()
+	}
+
+	if state.ws.Err != nil || !state.ws.Connected {
+		if state.ws.Err != nil {
+			state.logger.Info("error on socket", log.Error(state.ws.Err))
+		}
+		if err := state.subscribeInstrument(context); err != nil {
+			return fmt.Errorf("error subscribing to instrument: %v", err)
+		}
+	}
+
+	return nil
+}
