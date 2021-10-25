d156dc92405bd04e563e15be430508a386ad9fe2
Laurent Meunier <laurmeun.business@gmail.com>
2021-10-25 07:03:20 +0200

    fix: increase oi delay

--- exchanges/fbinance/fbinance_listener.go
+++ exchanges/fbinance/fbinance_listener.go
@@ -500,8 +500,8 @@ func (state *Listener) onMarketStatisticsResponse(context actor.Context) error {
 	// Reduce delay
 	oidLock.Lock()
 	oid = time.Duration(float64(oid) * 0.99)
-	if oid < 3*time.Second {
-		oid = 3 * time.Second
+	if oid < 5*time.Second {
+		oid = 5 * time.Second
 	}
 	state.openInterestTicker.Reset(oid)
 	oidLock.Unlock()
