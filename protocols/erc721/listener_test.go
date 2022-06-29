05fef04a85a3a0aef1ab91d087c52858e08b021d
Gregory Edison <gregory.edison1993@gmail.com>
2022-06-29 16:39:26 +0200

    test: erc721 svm listener

--- protocols/erc721/listener_test.go
+++ protocols/erc721/listener_test.go
@@ -9,6 +9,7 @@ import (
 	registry "gitlab.com/alphaticks/alpha-public-registry-grpc"
 	"gitlab.com/alphaticks/xchanger/constants"
 	"google.golang.org/grpc"
+	"reflect"
 	"testing"
 	"time"
 
@@ -19,7 +20,7 @@ import (
 	xchangerModels "gitlab.com/alphaticks/xchanger/models"
 )
 
-func TestListener(t *testing.T) {
+func TestListenerEVM(t *testing.T) {
 	exTests.LoadStatics(t)
 	protocol := constants.ERC721
 	registryAddress := "registry.alphaticks.io:8001"
@@ -64,7 +65,84 @@ func TestListener(t *testing.T) {
 	}
 	props := actor.PropsFromProducer(tests.NewERC721CheckerProducer(asset))
 	checker := as.Root.Spawn(props)
-	time.Sleep(800 * time.Second)
-	as.Root.Poison(checker)
-	time.Sleep(10 * time.Second)
+	defer as.Root.PoisonFuture(checker)
+	time.Sleep(10 * time.Minute)
+	resp, err := as.Root.RequestFuture(checker, &tests.GetDataRequest{}, 10*time.Second).Result()
+	if err != nil {
+		t.Fatal(err)
+	}
+	d, ok := resp.(*tests.GetDataResponse)
+	if !ok {
+		t.Fatalf("expected *tests.GetDataResponse, got %s", reflect.TypeOf(resp).String())
+	}
+	if d.Err != nil {
+		t.Fatal(d.Err)
+	}
+	for _, ev := range d.Updates {
+		fmt.Println(ev)
+	}
+}
+
+func TestListenerSVM(t *testing.T) {
+	exTests.LoadStatics(t)
+	protocol := constants.ERC721
+	registryAddress := "127.0.0.1:8001"
+	conn, err := grpc.Dial(registryAddress, grpc.WithInsecure())
+	if err != nil {
+		t.Fatal(err)
+	}
+	reg := registry.NewPublicRegistryClient(conn)
+
+	prCfg := &types.ExecutorConfig{
+		Registry:  reg,
+		Protocols: []*xchangerModels.Protocol{protocol},
+	}
+	as, ex, clean := exTests.StartExecutor(t, &xtypes.ExecutorConfig{}, prCfg, &ctypes.ExecutorConfig{}, nil)
+	defer clean()
+
+	res, err := as.Root.RequestFuture(ex, &messages.ProtocolAssetListRequest{
+		RequestID: uint64(time.Now().UnixNano()),
+		Subscribe: false,
+	}, 20*time.Second).Result()
+	if err != nil {
+		t.Fatal()
+	}
+	response, ok := res.(*messages.ProtocolAssetList)
+	if !ok {
+		t.Fatal("incorrect type assertion")
+	}
+	fmt.Println(response.ProtocolAssets)
+	assetTest := models.ProtocolAsset{
+		Asset: &xchangerModels.Asset{
+			ID: 275,
+		},
+	}
+	var asset *models.ProtocolAsset
+	chain, ok := constants.GetChainByID(5)
+	if !ok {
+		t.Fatal("missing chain")
+	}
+	for _, a := range response.ProtocolAssets {
+		if assetTest.Asset.ID == a.Asset.ID && a.Chain.ID == chain.ID {
+			asset = a
+		}
+	}
+	if asset == nil {
+		t.Fatal("asset not found")
+	}
+	props := actor.PropsFromProducer(tests.NewERC721CheckerProducer(asset))
+	checker := as.Root.Spawn(props)
+	defer as.Root.PoisonFuture(checker)
+	time.Sleep(3 * time.Minute)
+	resp, err := as.Root.RequestFuture(checker, &tests.GetDataRequest{}, 10*time.Second).Result()
+	if err != nil {
+		t.Fatal(err)
+	}
+	d, ok := resp.(*tests.GetDataResponse)
+	if !ok {
+		t.Fatalf("expected *tests.GetDataResponse, got %s", reflect.TypeOf(resp).String())
+	}
+	if d.Err != nil {
+		t.Fatal(d.Err)
+	}
 }
