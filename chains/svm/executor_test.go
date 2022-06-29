f34b051534dce6b990b9173016579510ae9f84ee
Gregory Edison <gregory.edison1993@gmail.com>
2022-06-29 10:40:21 +0200

    test: stark transfer events query

--- chains/svm/executor_test.go
+++ chains/svm/executor_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/alphaticks/alpha-connect/chains/tests"
 	"gitlab.com/alphaticks/alpha-connect/models/messages"
-	"gitlab.com/alphaticks/xchanger/chains/starknet"
+	"gitlab.com/alphaticks/xchanger/chains/svm"
 	models2 "gitlab.com/alphaticks/xchanger/models"
 	"math/big"
 	"os"
@@ -53,43 +53,62 @@ func TestExecutorBlockNumberRequest(t *testing.T) {
 func TestExecutorSVMEventsQueryRequest(t *testing.T) {
 	v := validator.New()
 	add := common.HexToHash("0x0276069eb59afc97d3f6dcd18ec236cbb3e71611324bfc6e86a6aee0851a65bb")
-	q := &starknet.EventQuery{
+	q := svm.EventQuery{
 		To:              big.NewInt(2790),
 		ContractAddress: &add,
 		PageSize:        1000,
 		PageNumber:      0,
 	}
-	done := false
-	for !done {
-		resp, err := as.Root.RequestFuture(ex, &messages.SVMEventsQueryRequest{
-			RequestID: uint64(time.Now().UnixNano()),
-			Query:     q,
-			Chain:     chain,
-		}, 10*time.Second).Result()
-		assert.Nil(t, err, "SVMEventsQueryRequest err: %v", err)
-		events, ok := resp.(*messages.SVMEventsQueryResponse)
-		assert.True(t, ok, "expected SVMEventsQueryRequest, got %s", reflect.TypeOf(resp).String())
-		assert.True(t, events.Success, "failed SVMEventsQueryRequest, got: %s", events.RejectionReason.String())
-		assert.Equal(t, len(events.Events.Events), 6, "expected more than 5 events")
+	resp, err := as.Root.RequestFuture(ex, &messages.SVMEventsQueryRequest{
+		RequestID: uint64(time.Now().UnixNano()),
+		Query:     q,
+		Chain:     chain,
+	}, 10*time.Second).Result()
+	assert.Nil(t, err, "SVMEventsQueryRequest err: %v", err)
+	events, ok := resp.(*messages.SVMEventsQueryResponse)
+	assert.True(t, ok, "expected SVMEventsQueryRequest, got %s", reflect.TypeOf(resp).String())
+	assert.True(t, events.Success, "failed SVMEventsQueryRequest, got: %s", events.RejectionReason.String())
+	assert.Equal(t, len(events.Events), 6, "expected more than 5 events")
 
-		err = v.Struct(events.Events)
+	for _, ev := range events.Events {
+		err = v.Struct(ev)
 		assert.Nil(t, err, "Validate struct err: %v", err)
-		for _, ev := range events.Events.Events {
-			err = v.Struct(ev)
-			assert.Nil(t, err, "Validate struct err: %v", err)
-		}
-		q.PageNumber = uint64(events.Events.PageNumber + 1)
-		done = events.Events.IsLastPage
 	}
+}
+
+func TestExecutorSVMEventsQueryRequestTransfer(t *testing.T) {
+	v := validator.New()
+	key := common.HexToHash("0x0099cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9")
+	q := svm.EventQuery{
+		To:       big.NewInt(2493),
+		Keys:     &[]common.Hash{key},
+		PageSize: 1000,
+	}
+	resp, err := as.Root.RequestFuture(ex, &messages.SVMEventsQueryRequest{
+		RequestID: uint64(time.Now().UnixNano()),
+		Query:     q,
+		Chain:     chain,
+	}, 2*time.Minute).Result()
+	assert.Nil(t, err, "SVMEventsQueryRequest err: %v", err)
+	events, ok := resp.(*messages.SVMEventsQueryResponse)
+	assert.True(t, ok, "expected SVMEventsQueryRequest, got %s", reflect.TypeOf(resp).String())
+	assert.True(t, events.Success, "failed SVMEventsQueryRequest, got: %s", events.RejectionReason.String())
+	assert.Equal(t, 1057, len(events.Events), "expected more than 5 events")
+
+	for _, ev := range events.Events {
+		err = v.Struct(ev)
+		assert.Nil(t, err, "Validate struct err: %v", err)
+	}
+	assert.Equal(t, len(events.Events), len(events.Times), "mismatched length")
 }
 
 func TestExecutorSVMBlockQueryRequest(t *testing.T) {
 	v := validator.New()
 	hash := common.HexToHash("0x69b96255bf7cc630ba99292ca1dd34130829fa3486fe44bfbb7f7aa13a4da29")
 
-	q := &starknet.BlockQuery{
+	q := &svm.BlockQuery{
 		BlockHash: &hash,
-		TxScope:   &starknet.FULL_TXN_AND_RECEIPTS,
+		TxScope:   &svm.FULL_TXN_AND_RECEIPTS,
 	}
 	resp, err := as.Root.RequestFuture(ex, &messages.SVMBlockQueryRequest{
 		RequestID: uint64(time.Now().UnixNano()),
@@ -110,7 +129,7 @@ func TestExecutorSVMBlockQueryRequest(t *testing.T) {
 	}
 
 	blockN := big.NewInt(1828)
-	q = &starknet.BlockQuery{
+	q = &svm.BlockQuery{
 		BlockNumber: blockN,
 	}
 	resp, err = as.Root.RequestFuture(ex, &messages.SVMBlockQueryRequest{
