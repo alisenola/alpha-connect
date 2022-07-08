9bef61c94bac1572f675001c8402a605cae6a6c1
Laurent Meunier <laurmeun.business@gmail.com>
2022-07-08 14:21:54 +0200

    chore: clean test

--- data/live_store_test.go
+++ data/live_store_test.go
@@ -1,14 +1,11 @@
 package data
 
 import (
-	"fmt"
 	"gitlab.com/alphaticks/alpha-connect/config"
 	"gitlab.com/alphaticks/alpha-connect/tests"
 	types "gitlab.com/alphaticks/tickstore-types"
-	"gitlab.com/alphaticks/tickstore-types/tickobjects"
 	"gitlab.com/alphaticks/xchanger/constants"
 	"testing"
-	"time"
 )
 
 func TestLiveQuery(t *testing.T) {
@@ -19,7 +16,7 @@ func TestLiveQuery(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	qs := types.NewQuerySettings(types.WithSelector(`SELECT OBPrice(AggregateOB(orderbook), "0.2") WHERE exchange="^binance|coinbasepro$" base="^BTC$" quote="^USDT$" GROUPBY base`))
+	qs := types.NewQuerySettings(types.WithSelector(`SELECT OBPrice(orderbook, "0.2") WHERE exchange="^binance$" base="^BTC$" quote="^USDT$" GROUPBY base`))
 	q, err := lt.NewQuery(qs)
 	if err != nil {
 		t.Fatal(err)
@@ -27,8 +24,7 @@ func TestLiveQuery(t *testing.T) {
 	i := 0
 	for i < 100 {
 		for q.Next() {
-			tick, to, gid := q.Read()
-			fmt.Println(tick, to.(tickobjects.Float64Object).Float64(), gid)
+			q.Read()
 			i += 1
 		}
 		if q.Err() != nil {
@@ -39,6 +35,4 @@ func TestLiveQuery(t *testing.T) {
 	if err := q.Close(); err != nil {
 		t.Fatal(err)
 	}
-
-	time.Sleep(10 * time.Second)
 }
