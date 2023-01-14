afa426eee8d1a5b6ea8b3a65dfba1b5c8a545b2c
Laurent Meunier <laurmeun.business@gmail.com>
2023-01-14 20:33:59 +0100

    feat: bias

--- modeling/base_models.go
+++ modeling/base_models.go
@@ -34,7 +34,7 @@ type MarketModel interface {
 
 type AllocationModel interface {
 	Model
-	GetAllocations(margin float64) map[uint64]float64
+	GetAllocations(margin, bias float64) map[uint64]float64
 	GetAllocationDelta(ID uint64, margin, bias, size, cost float64) (float64, bool)
 }
 
