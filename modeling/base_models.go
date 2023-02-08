36675f0152d85d4c5de1ce32ec2d9869c9b03db0
Laurent Meunier <laurmeun.business@gmail.com>
2023-02-08 10:07:19 +0100

    feat: Optional gamma on allocation model

--- modeling/base_models.go
+++ modeling/base_models.go
@@ -36,8 +36,8 @@ type AllocationModel interface {
 	Model
 	Solve(allocs, limits *sync.Map, margin float64) error
 	Solved() bool
-	GetAllocations(allocations *sync.Map, margin, cost float64) map[uint64]float64
-	GetAllocationDelta(ID uint64, allocations *sync.Map, margin, cost float64) (float64, bool)
+	GetAllocations(allocations *sync.Map, margin, cost float64, gamma *float64) map[uint64]float64
+	GetAllocationDelta(ID uint64, allocations *sync.Map, margin, cost float64, gamma *float64) (float64, bool)
 }
 
 type LongShortModel interface {
