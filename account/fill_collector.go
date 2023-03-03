2e2d0a41cd65961fb69c44f184ee074948449327
Laurent Meunier <laurmeun.business@gmail.com>
2023-03-03 12:00:14 +0100

    fix: improve averaging

--- account/fill_collector.go
+++ account/fill_collector.go
@@ -19,10 +19,10 @@ type FillCollector struct {
 	cutoff         int64 // drop trades older than this
 	takerFills     map[uint64]*list.List
 	makerFills     map[uint64]*list.List
-	buyMakerMoves  map[uint64]map[int64]float64
-	buyTakerMoves  map[uint64]map[int64]float64
-	sellMakerMoves map[uint64]map[int64]float64
-	sellTakerMoves map[uint64]map[int64]float64
+	buyMakerMoves  map[uint64]map[int64][2]float64
+	buyTakerMoves  map[uint64]map[int64][2]float64
+	sellMakerMoves map[uint64]map[int64][2]float64
+	sellTakerMoves map[uint64]map[int64][2]float64
 }
 
 func NewFillCollector(cutoff int64, securityIDs []uint64) *FillCollector {
@@ -30,18 +30,18 @@ func NewFillCollector(cutoff int64, securityIDs []uint64) *FillCollector {
 		cutoff:         cutoff,
 		takerFills:     make(map[uint64]*list.List),
 		makerFills:     make(map[uint64]*list.List),
-		buyMakerMoves:  make(map[uint64]map[int64]float64),
-		buyTakerMoves:  make(map[uint64]map[int64]float64),
-		sellMakerMoves: make(map[uint64]map[int64]float64),
-		sellTakerMoves: make(map[uint64]map[int64]float64),
+		buyMakerMoves:  make(map[uint64]map[int64][2]float64),
+		buyTakerMoves:  make(map[uint64]map[int64][2]float64),
+		sellMakerMoves: make(map[uint64]map[int64][2]float64),
+		sellTakerMoves: make(map[uint64]map[int64][2]float64),
 	}
 	for _, sec := range securityIDs {
 		fc.takerFills[sec] = list.New()
 		fc.makerFills[sec] = list.New()
-		fc.buyMakerMoves[sec] = make(map[int64]float64)
-		fc.buyTakerMoves[sec] = make(map[int64]float64)
-		fc.sellMakerMoves[sec] = make(map[int64]float64)
-		fc.sellTakerMoves[sec] = make(map[int64]float64)
+		fc.buyMakerMoves[sec] = make(map[int64][2]float64)
+		fc.buyTakerMoves[sec] = make(map[int64][2]float64)
+		fc.sellMakerMoves[sec] = make(map[int64][2]float64)
+		fc.sellTakerMoves[sec] = make(map[int64][2]float64)
 	}
 	return fc
 }
@@ -60,7 +60,7 @@ func (sc *FillCollector) Collect(securityID uint64, price float64) {
 	sc.Lock()
 	defer sc.Unlock()
 	ts := time.Now().UnixMilli()
-	alpha := 0.995
+	alpha := 0.5
 	m := sc.makerFills[securityID]
 	for e := m.Front(); e != nil; e = e.Next() {
 		f := e.Value.(*fill)
@@ -80,17 +80,17 @@ func (sc *FillCollector) Collect(securityID uint64, price float64) {
 				if f.buy {
 					buyMoves, ok := sc.buyMakerMoves[securityID]
 					if !ok {
-						buyMoves = make(map[int64]float64)
+						buyMoves = make(map[int64][2]float64)
 						sc.buyMakerMoves[securityID] = buyMoves
 					}
-					buyMoves[bucket] = alpha*buyMoves[bucket] + (1-alpha)*move
+					buyMoves[bucket] = [2]float64{alpha*buyMoves[bucket][0] + (1-alpha)*move, float64(ts)}
 				} else {
 					sellMoves, ok := sc.sellMakerMoves[securityID]
 					if !ok {
-						sellMoves = make(map[int64]float64)
+						sellMoves = make(map[int64][2]float64)
 						sc.sellMakerMoves[securityID] = sellMoves
 					}
-					sellMoves[bucket] = alpha*sellMoves[bucket] + (1-alpha)*move
+					sellMoves[bucket] = [2]float64{alpha*sellMoves[bucket][0] + (1-alpha)*move, float64(ts)}
 				}
 				f.bucketed[bucket] = true
 			}
@@ -114,17 +114,17 @@ func (sc *FillCollector) Collect(securityID uint64, price float64) {
 				if f.buy {
 					buyMoves, ok := sc.buyTakerMoves[securityID]
 					if !ok {
-						buyMoves = make(map[int64]float64)
+						buyMoves = make(map[int64][2]float64)
 						sc.buyTakerMoves[securityID] = buyMoves
 					}
-					buyMoves[bucket] = alpha*buyMoves[bucket] + (1-alpha)*move
+					buyMoves[bucket] = [2]float64{alpha*buyMoves[bucket][0] + (1-alpha)*move, float64(ts)}
 				} else {
 					sellMoves, ok := sc.sellTakerMoves[securityID]
 					if !ok {
-						sellMoves = make(map[int64]float64)
+						sellMoves = make(map[int64][2]float64)
 						sc.sellTakerMoves[securityID] = sellMoves
 					}
-					sellMoves[bucket] = alpha*sellMoves[bucket] + (1-alpha)*move
+					sellMoves[bucket] = [2]float64{alpha*sellMoves[bucket][0] + (1-alpha)*move, float64(ts)}
 				}
 				f.bucketed[bucket] = true
 			}
@@ -140,52 +140,57 @@ func (sc *FillCollector) GetMoveAfterFill() ([]float64, []float64, []float64, []
 	buyTakerMoves := make([]float64, 10)
 	sellMakerMoves := make([]float64, 10)
 	sellTakerMoves := make([]float64, 10)
+	ts := float64(time.Now().UnixMilli())
 	for i := 0; i < 10; i++ {
-		sum := 0
+		sum := 0.
 		for _, v := range sc.buyMakerMoves {
 			if mv, ok := v[int64(i)]; ok {
-				buyMakerMoves[i] += mv
-				sum += 1
+				w := 1 / (ts - mv[1])
+				buyMakerMoves[i] += mv[0] * w
+				sum += w
 			}
 		}
-		if sum != 0 {
-			buyMakerMoves[i] /= float64(sum)
+		if sum != 0. {
+			buyMakerMoves[i] /= sum
 		}
 	}
 	for i := 0; i < 10; i++ {
-		sum := 0
+		sum := 0.
 		for _, v := range sc.buyTakerMoves {
 			if mv, ok := v[int64(i)]; ok {
-				buyTakerMoves[i] += mv
-				sum += 1
+				w := 1 / (ts - mv[1])
+				buyTakerMoves[i] += mv[0] * w
+				sum += w
 			}
 		}
-		if sum != 0 {
-			buyTakerMoves[i] /= float64(sum)
+		if sum != 0. {
+			buyTakerMoves[i] /= sum
 		}
 	}
 	for i := 0; i < 10; i++ {
-		sum := 0
+		sum := 0.
 		for _, v := range sc.sellMakerMoves {
 			if mv, ok := v[int64(i)]; ok {
-				sellMakerMoves[i] += mv
-				sum += 1
+				w := 1 / (ts - mv[1])
+				sellMakerMoves[i] += mv[0] * w
+				sum += w
 			}
 		}
 		if sum != 0 {
-			sellMakerMoves[i] /= float64(sum)
+			sellMakerMoves[i] /= sum
 		}
 	}
 	for i := 0; i < 10; i++ {
-		sum := 0
+		sum := 0.
 		for _, v := range sc.sellTakerMoves {
 			if mv, ok := v[int64(i)]; ok {
-				sellTakerMoves[i] += mv
-				sum += 1
+				w := 1 / (ts - mv[1])
+				sellTakerMoves[i] += mv[0] * w
+				sum += w
 			}
 		}
 		if sum != 0 {
-			sellTakerMoves[i] /= float64(sum)
+			sellTakerMoves[i] /= sum
 		}
 	}
 	return buyMakerMoves, buyTakerMoves, sellMakerMoves, sellTakerMoves
