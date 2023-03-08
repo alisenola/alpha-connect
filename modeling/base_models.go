891ae4ad2bbaf57abd5d2e5180c69030d5fbeb67
Laurent Meunier <laurmeun.business@gmail.com>
2023-03-08 08:52:10 +0100

    feat: solve

--- modeling/base_models.go
+++ modeling/base_models.go
@@ -34,7 +34,7 @@ type MarketModel interface {
 
 type AllocationModel interface {
 	Model
-	Solve(allocs, limits *sync.Map, margin float64) error
+	Solve(allocs, bidDepths, askDepths, limits *sync.Map, margin float64, gamma *float64) error
 	Solved() bool
 	GetAllocations(allocations *sync.Map, margin, cost float64, gamma *float64) map[uint64]float64
 	GetAllocationDelta(ID uint64, allocations *sync.Map, margin, cost float64, gamma *float64) (float64, bool)
